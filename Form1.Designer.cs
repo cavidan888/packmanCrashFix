cb47b78cfc3fb1fe6d55fbc0085ba708a1a6c1af
cavidan888 <cavidan666@gmail.com>
2018-07-06 12:40:56 +0400

    pack

---
+++ Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace Packman_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startGame = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.obs = new System.Windows.Forms.Label();
+            this.obs4 = new System.Windows.Forms.Label();
+            this.obs3 = new System.Windows.Forms.Label();
+            this.obs2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.midObs2 = new System.Windows.Forms.Label();
+            this.midObs5 = new System.Windows.Forms.Label();
+            this.misObs7 = new System.Windows.Forms.Label();
+            this.midObs1 = new System.Windows.Forms.Label();
+            this.midObs3 = new System.Windows.Forms.Label();
+            this.midObs4 = new System.Windows.Forms.Label();
+            this.packman = new System.Windows.Forms.Button();
+            this.meal = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.midObs6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startGame
+            // 
+            this.startGame.Location = new System.Drawing.Point(541, 481);
+            this.startGame.Name = "startGame";
+            this.startGame.Size = new System.Drawing.Size(75, 23);
+            this.startGame.TabIndex = 1;
+            this.startGame.Text = "Start";
+            this.startGame.UseVisualStyleBackColor = true;
+            this.startGame.Click += new System.EventHandler(this.startGame_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // obs
+            // 
+            this.obs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.obs.Location = new System.Drawing.Point(24, 23);
+            this.obs.Name = "obs";
+            this.obs.Size = new System.Drawing.Size(21, 412);
+            this.obs.TabIndex = 0;
+            // 
+            // obs4
+            // 
+            this.obs4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.obs4.Location = new System.Drawing.Point(44, 413);
+            this.obs4.Name = "obs4";
+            this.obs4.Size = new System.Drawing.Size(518, 22);
+            this.obs4.TabIndex = 0;
+            // 
+            // obs3
+            // 
+            this.obs3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.obs3.Location = new System.Drawing.Point(42, 23);
+            this.obs3.Name = "obs3";
+            this.obs3.Size = new System.Drawing.Size(520, 22);
+            this.obs3.TabIndex = 0;
+            // 
+            // obs2
+            // 
+            this.obs2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.obs2.Location = new System.Drawing.Point(562, 23);
+            this.obs2.Name = "obs2";
+            this.obs2.Size = new System.Drawing.Size(21, 412);
+            this.obs2.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Yellow;
+            this.label5.Location = new System.Drawing.Point(94, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 15);
+            this.label5.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Yellow;
+            this.label8.Location = new System.Drawing.Point(425, 80);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 15);
+            this.label8.TabIndex = 1;
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.Yellow;
+            this.label10.Location = new System.Drawing.Point(94, 358);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(95, 15);
+            this.label10.TabIndex = 1;
+            // 
+            // label12
+            // 
+            this.label12.BackColor = System.Drawing.Color.Yellow;
+            this.label12.Location = new System.Drawing.Point(425, 360);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(95, 15);
+            this.label12.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Yellow;
+            this.label6.Location = new System.Drawing.Point(94, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 72);
+            this.label6.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Yellow;
+            this.label9.Location = new System.Drawing.Point(503, 90);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(17, 72);
+            this.label9.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Yellow;
+            this.label7.Location = new System.Drawing.Point(94, 294);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 79);
+            this.label7.TabIndex = 1;
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.Yellow;
+            this.label11.Location = new System.Drawing.Point(503, 296);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(17, 79);
+            this.label11.TabIndex = 1;
+            // 
+            // midObs2
+            // 
+            this.midObs2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.midObs2.Location = new System.Drawing.Point(311, 112);
+            this.midObs2.Name = "midObs2";
+            this.midObs2.Size = new System.Drawing.Size(17, 72);
+            this.midObs2.TabIndex = 2;
+            // 
+            // midObs5
+            // 
+            this.midObs5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.midObs5.Location = new System.Drawing.Point(195, 187);
+            this.midObs5.Name = "midObs5";
+            this.midObs5.Size = new System.Drawing.Size(17, 72);
+            this.midObs5.TabIndex = 2;
+            // 
+            // misObs7
+            // 
+            this.misObs7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.misObs7.Location = new System.Drawing.Point(231, 218);
+            this.misObs7.Name = "misObs7";
+            this.misObs7.Size = new System.Drawing.Size(138, 18);
+            this.misObs7.TabIndex = 2;
+            // 
+            // midObs1
+            // 
+            this.midObs1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.midObs1.Location = new System.Drawing.Point(270, 112);
+            this.midObs1.Name = "midObs1";
+            this.midObs1.Size = new System.Drawing.Size(17, 72);
+            this.midObs1.TabIndex = 2;
+            // 
+            // midObs3
+            // 
+            this.midObs3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.midObs3.Location = new System.Drawing.Point(270, 261);
+            this.midObs3.Name = "midObs3";
+            this.midObs3.Size = new System.Drawing.Size(17, 72);
+            this.midObs3.TabIndex = 4;
+            // 
+            // midObs4
+            // 
+            this.midObs4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.midObs4.Location = new System.Drawing.Point(321, 261);
+            this.midObs4.Name = "midObs4";
+            this.midObs4.Size = new System.Drawing.Size(17, 72);
+            this.midObs4.TabIndex = 3;
+            // 
+            // packman
+            // 
+            this.packman.BackColor = System.Drawing.Color.Blue;
+            this.packman.Enabled = false;
+            this.packman.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.packman.Location = new System.Drawing.Point(117, 97);
+            this.packman.Name = "packman";
+            this.packman.Size = new System.Drawing.Size(20, 19);
+            this.packman.TabIndex = 5;
+            this.packman.UseVisualStyleBackColor = false;
+            // 
+            // meal
+            // 
+            this.meal.BackColor = System.Drawing.Color.Yellow;
+            this.meal.Enabled = false;
+            this.meal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.meal.Location = new System.Drawing.Point(428, 272);
+            this.meal.Name = "meal";
+            this.meal.Size = new System.Drawing.Size(29, 25);
+            this.meal.TabIndex = 6;
+            this.meal.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.midObs6);
+            this.groupBox1.Controls.Add(this.meal);
+            this.groupBox1.Controls.Add(this.packman);
+            this.groupBox1.Controls.Add(this.midObs4);
+            this.groupBox1.Controls.Add(this.midObs3);
+            this.groupBox1.Controls.Add(this.midObs1);
+            this.groupBox1.Controls.Add(this.misObs7);
+            this.groupBox1.Controls.Add(this.midObs5);
+            this.groupBox1.Controls.Add(this.midObs2);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.obs2);
+            this.groupBox1.Controls.Add(this.obs3);
+            this.groupBox1.Controls.Add(this.obs4);
+            this.groupBox1.Controls.Add(this.obs);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Location = new System.Drawing.Point(33, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(617, 452);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // midObs6
+            // 
+            this.midObs6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.midObs6.Enabled = false;
+            this.midObs6.Location = new System.Drawing.Point(386, 187);
+            this.midObs6.Name = "midObs6";
+            this.midObs6.Size = new System.Drawing.Size(17, 72);
+            this.midObs6.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 511);
+            this.Controls.Add(this.startGame);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button startGame;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label obs;
+        private System.Windows.Forms.Label obs4;
+        private System.Windows.Forms.Label obs3;
+        private System.Windows.Forms.Label obs2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label midObs2;
+        private System.Windows.Forms.Label midObs5;
+        private System.Windows.Forms.Label misObs7;
+        private System.Windows.Forms.Label midObs1;
+        private System.Windows.Forms.Label midObs3;
+        private System.Windows.Forms.Label midObs4;
+        private System.Windows.Forms.Button packman;
+        private System.Windows.Forms.Button meal;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label midObs6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Packman_Game
+{
+    public partial class Form1 : Form
+    {
+
+        public int leftPos = 2;
+        public int topPos = 2;
+        public int count = 0;
+        public int findTimer = 0;
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            packman.Left = 120;
+            packman.Top = 100;
+
+        }
+
+
+       
+
+        private void startGame_Click(object sender, EventArgs e)
+        {
+            count++;
+            timer1.Start();
+
+            if (count > 0)
+            {
+                startGame.Enabled = false;
+            }
+           
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            packman.Left += leftPos;
+            timer1.Interval = 60;
+           
+
+            if (findTimer == 1)
+            {
+                packman.Left -= leftPos;
+                packman.Top += topPos;
+
+                if (packman.Location.Y >= obs4.Location.Y - packman.Width)
+                {
+                    timer1.Enabled = false;
+                    packman.Left = 120;
+                    packman.Top = 100;
+                    startGame.Enabled = true;
+                }
+
+                
+            }
+            else if (findTimer == 2)
+            {
+                packman.Left -= leftPos;
+                packman.Top -= topPos;
+
+
+                if (packman.Location.Y <= obs3.Location.Y + packman.Width)
+                {
+                    timer1.Enabled = false;
+                    MessageBox.Show(packman.Location.ToString());
+                }
+                
+            }
+            else if (findTimer == 3)
+            {
+
+                packman.Left -= leftPos;
+                packman.Left += leftPos;
+
+                if (packman.Location.X >= obs2.Location.X-packman.Width)
+                {
+                    timer1.Enabled = false;
+                    MessageBox.Show(packman.Location.ToString());
+                }
+                
+
+
+            }
+            else if (findTimer == 4)
+            {
+                packman.Left -= leftPos + 1;
+
+                if (packman.Location.X <= obs.Location.X+packman.Width)
+                {
+                    timer1.Enabled = false;
+                    MessageBox.Show(packman.Location.ToString());
+                }
+
+               
+            }
+        }
+
+
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                findTimer = 1;
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                findTimer = 2;
+
+                
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                findTimer = 3;
+
+
+               
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                findTimer = 4;
+
+                
+            }
+
+          
+        }
+
+
+
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
